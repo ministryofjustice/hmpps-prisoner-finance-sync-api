44031f2b4fd12b3c27141704ec06b63ba6a40259
Pietro Convalle <pietro.convalle@madetech.com>
2025-11-24 14:44:17 +0000

    Created DomainEvent Handler and dataclasses Added LocalStack to mock aws locally Added basic unit test for domain events Updated Readme

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/services/domainevents/PrisonerEvent.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.services.domainevents
+
+import org.springframework.stereotype.Component
+
+@Component
+class PrisonerEvent {
+
+  fun mergeAccounts(accountFrom: String, accountTo: String) {
+    return
+  }
+}
