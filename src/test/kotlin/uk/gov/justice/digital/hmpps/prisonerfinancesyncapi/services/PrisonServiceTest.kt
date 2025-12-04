41c61182f6a93e65206cb922dcffe04c6010c5cc
tristan-madetech <tristan.moss@justice.gov.uk>
2025-12-04 10:47:51 +0000

    Update PrisonServiceTest.kt
    Updates based on feedback

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/services/PrisonServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/services/PrisonServiceTest.kt
@@ -4,15 +4,16 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.any
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.jpa.entities.Prison
 import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.jpa.repositories.PrisonRepository
@@ -35,8 +36,6 @@ class PrisonServiceTest {
 
   private lateinit var dummyPrison: Prison
 
-  private val dummyPrisonId: String = "CCC"
-
   @BeforeEach
   fun setupGlobalDummies() {
     dummyPrison = Prison(
@@ -46,36 +45,55 @@ class PrisonServiceTest {
     )
   }
 
-  @Test
-  fun `should find payload if found by prison code`() {
-    `when`(prisonService.getPrison(Mockito.anyString())).thenReturn(dummyPrison)
+  @Nested
+  @DisplayName("getPrison")
+  inner class GetPrison {
+    @Test
+    fun `should return prison when found by prison code`() {
+      `when`(prisonRepository.findByCode(dummyPrison.code)).thenReturn(dummyPrison)
 
-    val result = prisonService.getPrison("AAA")
+      val prison = prisonService.getPrison(dummyPrison.code)
 
-    assertThat(result).isNotNull()
-    assertThat(result?.id).isNotNull()
-  }
-
-  @Test
-  fun `should return null if not found by prison code`() {
-    `when`(prisonService.getPrison(Mockito.anyString())).thenReturn(null)
-
-    val result = prisonService.getPrison("BBB")
-
-    assertThat(result).isNull()
-  }
-
-  @Test
-  fun `should persist new prison and assign generated ID`() {
-    whenever(prisonRepository.save(any())).thenAnswer { invocation ->
-      val prisonResult = invocation.getArgument<Prison>(0)
-      prisonResult.copy(id = 1L)
+      assertThat(prison).isEqualTo(dummyPrison)
+      verify(prisonRepository).findByCode(dummyPrison.code)
     }
 
-    val savedPrison = prisonService.createPrison(dummyPrisonId)
+    @Test
+    fun `should return null when not found by prison code`() {
+      `when`(prisonRepository.findByCode(dummyPrison.code)).thenReturn(null)
 
-    assertEquals(1L, savedPrison.id)
-    assertEquals(dummyPrisonId, savedPrison.code)
-    verify(prisonRepository).save(any())
+      val result = prisonService.getPrison(dummyPrison.code)
+
+      assertThat(result).isNull()
+    }
+  }
+
+  @Nested
+  @DisplayName("createPrison")
+  inner class CreatePrison {
+
+    @Test
+    fun `should persist new prison and assign generated ID`() {
+      val savedPrisonId = 999L
+      val dummyPrisonId: String = "CCC"
+
+      whenever(prisonRepository.save(any())).thenAnswer { invocation ->
+        val prisonResult = invocation.getArgument<Prison>(0)
+        prisonResult.copy(id = savedPrisonId)
+      }
+
+      val savedPrison = prisonService.createPrison(dummyPrisonId)
+
+      assertEquals(savedPrisonId, savedPrison.id)
+      assertEquals(dummyPrisonId, savedPrison.code)
+
+      verify(accountService).createGeneralLedgerAccount(savedPrisonId, 2101)
+      verify(accountService).createGeneralLedgerAccount(savedPrisonId, 2102)
+      verify(accountService).createGeneralLedgerAccount(savedPrisonId, 2103)
+
+      verifyNoMoreInteractions(accountService)
+
+      verify(prisonRepository).save(any())
+    }
   }
 }
