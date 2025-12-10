c29bf8a90eabb32de0c52c680c7fa5e106f0becd
jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>
2025-12-10 15:59:22 +0000

    wip

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/services/PrisonerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/services/PrisonerService.kt
@@ -1,11 +1,39 @@
 package uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.services
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.jpa.repositories.AccountRepository
+import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.jpa.repositories.TransactionEntryRepository
 
 @Component
-class PrisonerService {
+class PrisonerService(
+  private val transactionEntryRepository: TransactionEntryRepository,
+  private val accountRepository: AccountRepository,
+) {
 
   fun merge(prisonNumberFrom: String, prisonNumberTo: String) {
+    val accountsToMerge = accountRepository.findByPrisonNumber(prisonNumberFrom)
+
+    if (accountsToMerge.isEmpty()) {
+      log.info("No accounts found for prisoner $prisonNumberFrom. ")
+      return
+    }
+
+    accountsToMerge.forEach { oldAccount ->
+
+      val targetAccount = accountRepository.findByPrisonNumberAndAccountCode(prisonNumberTo, oldAccount.accountCode)
+
+      if (targetAccount != null) {
+        log.info("Merge account ${oldAccount.accountCode} ${oldAccount.name} to ${targetAccount.accountCode} ${targetAccount.name}")
+
+        // transactionEntryRepository.reassignEntries(oldAccount.id, targetAccount.id)
+      }
+    }
+
     return
+  }
+
+  private companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
   }
 }
