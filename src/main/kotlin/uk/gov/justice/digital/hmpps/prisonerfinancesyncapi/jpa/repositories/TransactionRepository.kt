766998c994e6f39d1879ce3bd95a9133a0e72e1a
tristan-madetech <tristan.moss@justice.gov.uk>
2026-01-06 13:34:58 +0000

    PFI-698 (#53)
    Modified Transaction Repository with new method to use SQL and Ledger Balance Service to use new method findAllByAccountId

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/jpa/repositories/TransactionRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/jpa/repositories/TransactionRepository.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.jpa.repositories
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.jpa.entities.Transaction
 import java.sql.Timestamp
@@ -8,6 +10,15 @@ import java.util.UUID
 
 @Repository
 interface TransactionRepository : JpaRepository<Transaction, Long> {
+  @Query(
+    """
+        SELECT DISTINCT t 
+        FROM Transaction t 
+        INNER JOIN TransactionEntry te ON t.id = te.transactionId 
+        WHERE te.accountId = :accountId
+    """,
+  )
+  fun findAllByAccountId(@Param("accountId") accountId: Long): List<Transaction>
   fun findByUuid(uuid: UUID): Transaction?
   fun findByDateBetween(dateStart: Timestamp, dateEnd: Timestamp): List<Transaction>
 }
