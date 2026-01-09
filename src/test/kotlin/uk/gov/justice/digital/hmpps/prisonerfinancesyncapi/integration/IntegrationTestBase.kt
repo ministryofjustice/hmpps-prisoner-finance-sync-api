0cb27f40b9bd75e9a574eee9b9c54aa6090eb370
tristan-madetech <tristan.moss@justice.gov.uk>
2026-01-09 15:32:03 +0000

    Initial check-in, working integration tests apart from nomis sync payload repository tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/integration/IntegrationTestBase.kt
@@ -9,6 +9,7 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.testcontainers.containers.PostgreSQLContainer
 import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.integration.config.LocalStackConfig
 import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.integration.wiremock.HmppsAuthApiExtension
 import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.integration.wiremock.HmppsAuthApiExtension.Companion.hmppsAuth
@@ -36,12 +37,22 @@ abstract class IntegrationTestBase {
   }
 
   companion object {
+
     private val localStackContainer = LocalStackConfig.instance
+
+    private val postgres = PostgreSQLContainer("postgres:18")
+      .withDatabaseName("testdb")
+      .withUsername("test2")
+      .withPassword("test2")
 
     @Suppress("unused")
     @JvmStatic
     @DynamicPropertySource
     fun testcontainers(registry: DynamicPropertyRegistry) {
+      postgres.start()
+      registry.add("spring.datasource.url", postgres::getJdbcUrl)
+      registry.add("spring.datasource.username", postgres::getUsername)
+      registry.add("spring.datasource.password", postgres::getPassword)
       localStackContainer?.also { LocalStackConfig.setLocalStackProperties(it, registry) }
     }
   }
