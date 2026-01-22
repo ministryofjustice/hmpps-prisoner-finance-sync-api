e72e917d6c430854743b0c261782760afbc20abc
Pietro Convalle <pietro.convalle@madetech.com>
2026-01-22 09:14:39 +0000

    reverted start and end date behaviour

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/jpa/repositories/NomisSyncPayloadRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/jpa/repositories/NomisSyncPayloadRepository.kt
@@ -28,11 +28,12 @@ interface NomisSyncPayloadRepository : JpaRepository<NomisSyncPayload, Long> {
           n.requestId as requestId,
           n.transactionTimestamp as transactionTimestamp
         FROM NomisSyncPayload n 
-        WHERE (:prisonId IS NULL OR n.caseloadId = :prisonId)
-        AND (n.timestamp between :startDate AND :endDate)
+        WHERE (:prisonId IS NULL OR n.caseloadId = :prisonId) AND
+              (:startDate IS NULL OR n.timestamp >= :startDate ) AND
+              (:endDate IS NULL OR n.timestamp >= :endDate ) 
     """,
   )
-  fun findByCaseloadIdAndDateRange(prisonId: String?, startDate: Instant, endDate: Instant, pageable: Pageable): Page<NomisSyncPayloadSummary>
+  fun findByCaseloadIdAndDateRange(prisonId: String?, startDate: Instant?, endDate: Instant?, pageable: Pageable): Page<NomisSyncPayloadSummary>
 
   @Query(
     """
