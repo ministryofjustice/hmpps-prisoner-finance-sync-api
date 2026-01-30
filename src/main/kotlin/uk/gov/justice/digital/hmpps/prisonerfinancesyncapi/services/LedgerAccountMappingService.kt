49f7d6a92e5befd535c319e7418cdc64bf7c9218
Pietro-MadeTech <pietro.convalle@madetech.com>
2026-01-30 10:02:49 +0000

    Ledger account mapping service (#90)
    * Add general ledger mapping service
    
    * Implemented NOMIS to GL mapping and reverse
    
    ---------
    
    Co-authored-by: jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/services/LedgerAccountMappingService.kt
@@ -0,0 +1,37 @@
+package uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.services
+import org.springframework.stereotype.Service
+
+@Service
+class LedgerAccountMappingService {
+
+  data class PrisonAccountDetails(val code: Int, val txType: String)
+
+  fun mapPrisonerSubAccount(nomisAccountCode: Int): String = when (nomisAccountCode) {
+    2101 -> "CASH"
+    2102 -> "SPENDS"
+    2103 -> "SAVINGS"
+    else -> throw IllegalArgumentException("Unknown NOMIS Prisoner Account Code: $nomisAccountCode")
+  }
+
+  fun mapPrisonSubAccount(nomisAccountCode: Int, transactionType: String): String = "$nomisAccountCode:$transactionType"
+
+  fun mapSubAccountGLReferenceToNOMIS(GLReferenceCode: String): PrisonAccountDetails {
+    val splitReference = GLReferenceCode.trim().split(":")
+
+    if (splitReference.size != 2) {
+      throw IllegalArgumentException("GL Reference code should only have 2 elements")
+    }
+
+    val code = splitReference[0].toIntOrNull()
+      ?: throw IllegalArgumentException("GL Reference code $GLReferenceCode is not a valid number")
+
+    return PrisonAccountDetails(code, splitReference[1].trim())
+  }
+
+  fun mapSubAccountPrisonerReferenceToNOMIS(referenceGLCode: String): Int = when (referenceGLCode) {
+    "CASH" -> 2101
+    "SPENDS" -> 2102
+    "SAVINGS" -> 2103
+    else -> throw IllegalArgumentException("Unknown GL Prisoner reference Code: $referenceGLCode")
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/services/LedgerAccountMappingServiceTest.kt
@@ -0,0 +1,117 @@
+package uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.services
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class LedgerAccountMappingServiceTest {
+
+  private val mappingService = LedgerAccountMappingService()
+
+  @Nested
+  @DisplayName("mapPrisonerSubAccount")
+  inner class MapPrisonerSubAccount {
+
+    @ParameterizedTest
+    @CsvSource(
+      "2101, CASH",
+      "2102, SPENDS",
+      "2103, SAVINGS",
+    )
+    fun `should map NOMIS prisoner account codes to GL account reference`(accountCode: Int, expectedReference: String) {
+      val result = mappingService.mapPrisonerSubAccount(accountCode)
+      assertThat(result).isEqualTo(expectedReference)
+    }
+
+    @Test
+    fun `should throw exception for unknown prisoner code`() {
+      val unknownCode = 9999
+      assertThatThrownBy {
+        mappingService.mapPrisonerSubAccount(unknownCode)
+      }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+  }
+
+  @Nested
+  @DisplayName("mapPrisonSubAccount")
+  inner class MapPrisonSubAccount {
+
+    @ParameterizedTest
+    @CsvSource(
+      "1502, ADV,  1502:ADV",
+      "2501, CANT, 2501:CANT",
+      "2199, HOA,  2199:HOA",
+      "2199, WHF,  2199:WHF",
+      "7000, FRED, 7000:FRED",
+    )
+    fun `should map NOMIS prison account code and transaction type to GL account reference`(
+      accountCode: Int,
+      txnType: String,
+      expectedReference: String,
+    ) {
+      val result = mappingService.mapPrisonSubAccount(accountCode, txnType)
+      assertThat(result).isEqualTo(expectedReference)
+    }
+  }
+
+  @Nested
+  @DisplayName("mapSubAccountGLReferenceToNOMIS")
+  inner class MapSubAccountGLReferenceToNOMIS {
+
+    @ParameterizedTest
+    @CsvSource(
+      "1502:ADV, 1502, ADV",
+      "2501:CANT, 2501, CANT",
+      "2199:HOA, 2199, HOA",
+      "2199:WHF, 2199, WHF",
+      "7000:FRED, 7000, FRED",
+    )
+    fun `should map GL account reference to NOMIS prison account code, and transaction type`(
+      inputString: String,
+      expectedCode: Int,
+      expectedTxType: String,
+    ) {
+      val result = mappingService.mapSubAccountGLReferenceToNOMIS(inputString)
+      assertThat(result.code).isEqualTo(expectedCode)
+      assertThat(result.txType).isEqualTo(expectedTxType)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+      "BAD:REF",
+      "BAD:REF:REF",
+    )
+    fun `should handle illegal GL references`(arg: String) {
+      assertThrows<IllegalArgumentException> { mappingService.mapSubAccountGLReferenceToNOMIS(arg) }
+    }
+  }
+
+  @Nested
+  @DisplayName("mapSubAccountPrisonerReferenceToNOMIS")
+  inner class MapSubAccountPrisonerReferenceToNOMIS {
+
+    @ParameterizedTest
+    @CsvSource(
+      "CASH, 2101",
+      "SPENDS, 2102",
+      "SAVINGS, 2103",
+    )
+    fun `should map NOMIS prisoner account codes to GL account reference`(inputReference: String, expectedCode: Int) {
+      val result = mappingService.mapSubAccountPrisonerReferenceToNOMIS(inputReference)
+      assertThat(result).isEqualTo(expectedCode)
+    }
+
+    @Test
+    fun `should throw exception for unknown prisoner code`() {
+      val unknownCode = "HELLOWORLD"
+      assertThatThrownBy {
+        mappingService.mapSubAccountPrisonerReferenceToNOMIS(unknownCode)
+      }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+  }
+}
