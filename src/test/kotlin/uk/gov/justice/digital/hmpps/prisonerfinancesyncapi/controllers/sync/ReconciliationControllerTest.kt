dd26cc0531d6b0ecaebf1ff2f01f9f8d35315a21
jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>
2026-02-04 17:10:26 +0000

    Fix failing test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/controllers/sync/ReconciliationControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/controllers/sync/ReconciliationControllerTest.kt
@@ -9,9 +9,13 @@ import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.verify
 import org.springframework.http.HttpStatusCode
 import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.services.ReconciliationService
+import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.services.ledger.LedgerQueryService
 
 @ExtendWith(MockitoExtension::class)
 class ReconciliationControllerTest {
+
+  @Mock
+  private lateinit var ledgerQueryService: LedgerQueryService
 
   @Mock
   private lateinit var reconciliationService: ReconciliationService
@@ -20,11 +24,20 @@ class ReconciliationControllerTest {
   private lateinit var reconciliationController: ReconciliationController
 
   @Test
-  fun `should call reconciliationService`() {
+  fun `should call reconcile prisoner when listing prisoners by establishment`() {
     val prisonNumber = "ABC123"
     val res = reconciliationController.listPrisonerBalancesByEstablishment(prisonNumber)
 
     assertThat(res.statusCode).isEqualTo(HttpStatusCode.valueOf(200))
     verify(reconciliationService).reconcilePrisoner(prisonNumber)
+  }
+
+  @Test
+  fun `should call ledger query service when listing prison balances`() {
+    val prisonId = "PRI"
+    val res = reconciliationController.listGeneralLedgerBalances(prisonId)
+
+    assertThat(res.statusCode).isEqualTo(HttpStatusCode.valueOf(200))
+    verify(ledgerQueryService).listGeneralLedgerBalances(prisonId)
   }
 }
