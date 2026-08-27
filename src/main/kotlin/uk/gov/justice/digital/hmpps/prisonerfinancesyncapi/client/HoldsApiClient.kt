0a3ba12d25db0a0d35521f08927ea9cf0759311a
emilyfbgov <emily.bennett@justice.gov.uk>
2026-08-27 15:07:11 +0100

    adds hold api client

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesyncapi/client/HoldsApiClient.kt
@@ -0,0 +1,56 @@
+package uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.client
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.client.GeneralLedgerApiClient.Companion.log
+import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.clients.holds.HoldsApi
+import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.config.CustomException
+import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.models.generalledger.CreateSubAccountRequest
+import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.models.holds.CreateHoldRequest
+import uk.gov.justice.digital.hmpps.prisonerfinancesyncapi.models.holds.HoldResponse
+
+class HoldsApiClient(
+  private val holdsApi: HoldsApi
+) {
+
+  private companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
+  private fun <T> handleExceptions(
+    block: () -> T,
+    message400: String = "Bad Request from General Ledger",
+    message404: String = "Not found",
+    message502: String = "Bad Gateway - General Ledger Unreachable or throwing an error",
+    message500: String = "Unexpected Error",
+  ): T {
+    try {
+      return block()
+    } catch (e: WebClientResponseException) {
+      when {
+        e.statusCode == HttpStatus.BAD_REQUEST && e.responseBodyAsString.contains("Page requested is out of range") ->
+          throw CustomException(message = "Page requested is out of range", status = HttpStatus.BAD_REQUEST)
+
+        e.statusCode == HttpStatus.BAD_REQUEST -> throw CustomException(message400, HttpStatus.BAD_REQUEST, e)
+
+        e.statusCode == HttpStatus.NOT_FOUND -> throw CustomException(message404, HttpStatus.NOT_FOUND, e)
+
+        e.statusCode == HttpStatus.INTERNAL_SERVER_ERROR -> throw CustomException(message502, HttpStatus.BAD_GATEWAY, e)
+
+        else -> throw CustomException(message500, HttpStatus.INTERNAL_SERVER_ERROR, e)
+      }
+    }
+  }
+
+  @Throws(WebClientResponseException::class)
+  fun postHolds(request: CreateHoldRequest): HoldResponse {
+    log.info("Creating Hold for hold number ${request.legacyHoldNumber} for prison number ${request.prisonNumber}")
+
+    return holdsApi.postHold( request)
+      .block()
+      ?: throw IllegalStateException("Received null response when creating hold ${request.legacyHoldNumber}")
+
+  }
+
+}
